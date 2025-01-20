cd0782b49ad5a5b9ae64b6ff39f9474d956982fe
shikhil <shikhilkrishnan2018@gmail.com>
2025-01-20 13:35:45 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectToDatabase } from "./config/connectDb";
+
+dotenv.config();
+
+const app: Express = express();
+ 
+const corsOption = {
+  origin: process.env.CORS,
+  method: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors(corsOption));
+
+
+
+const PORT = process.env.PORT || 3000;
+const startServer = async (): Promise<void> => {
+  try {
+    await connectToDatabase();
+    app.listen(PORT, () => {
+      console.log(`server running on port${PORT}`);
+    });
+  } catch (error) {
+    console.error("error:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
